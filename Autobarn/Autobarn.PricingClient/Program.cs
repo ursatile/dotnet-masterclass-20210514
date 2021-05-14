7db59387681e52a89515151bc83582a748813b9b
Dylan Beattie <dylan@dylanbeattie.net>
2021-05-14 14:29:52 +0100

    gRPC working

--- Autobarn/Autobarn.PricingClient/Program.cs
+++ Autobarn/Autobarn.PricingClient/Program.cs
@@ -1,24 +1,35 @@
 ﻿using System;
 using Grpc.Net.Client;
+using EasyNetQ;
 using Autobarn.PricingServer;
+using Autobarn.Messages;
 
 namespace Autobarn.PricingClient {
 	class Program {
 		const string GRPC = "https://workshop.ursatile.com:5003";
+		const string AMQP = "amqps://uzvpuvak:ozcsROQDKpXnTCmOwVV5AWCNFShiHbeD@rattlesnake.rmq.cloudamqp.com/uzvpuvak";
+		static Pricer.PricerClient pricer;
+		
 		static void Main(string[] args) {
 			using var channel = GrpcChannel.ForAddress(GRPC);
-			var client = new Pricer.PricerClient(channel);
-			while (true) {
-				var request = new PriceRequest {
-					Color = "blue",
-					Year = 1985,
-					Make = "Ford",
-					Model = "Mustang"
-				};
-				var reply = client.GetPrice(request);
-				Console.WriteLine($"{reply.Price} {reply.CurrencyCode}");
-				Console.ReadKey();
-			}
+			pricer = new Pricer.PricerClient(channel);
+			using var bus = RabbitHutch.CreateBus(AMQP);
+
+            bus.PubSub.Subscribe<NewCarMessage>("autobarn.pricingclient", HandleNewCarMessage);
+            Console.ReadKey();
 		}
+
+        static void HandleNewCarMessage(NewCarMessage message) {
+			var priceRequest = new PriceRequest {
+				Make = message.Make,
+				Model = message.Model,
+				Color = message.Color,
+				Year = message.Year
+			};
+			var price = pricer.GetPrice(priceRequest);
+			Console.WriteLine("Priced car: ");
+			Console.WriteLine($"{message.Make} {message.Model} ({message.Color}, {message.Year}");
+			Console.WriteLine($"Price: {price.Price} {price.CurrencyCode}");
+        }
 	}
 }
--- Autobarn/Autobarn.PricingServer/Services/PricerService.cs
+++ Autobarn/Autobarn.PricingServer/Services/PricerService.cs
@@ -7,14 +7,26 @@ using Microsoft.Extensions.Logging;
 
 namespace Autobarn.PricingServer {
 	public class PricerService : Pricer.PricerBase {
+        static int prices = 0;
+		static Random random = new Random();
+
 		private readonly ILogger<PricerService> _logger;
 		public PricerService(ILogger<PricerService> logger) {
 			_logger = logger;
 		}
 
 		public override Task<PriceReply> GetPrice(PriceRequest request, ServerCallContext context) {
+            _logger.Log(LogLevel.Information, $"{prices++} prices calculated!");
+			int price = 0;
+			if (request.Color.Contains("blue", StringComparison.OrdinalIgnoreCase)) {
+				price = 200;
+			} else if (request.Make == "Tesla") {
+				price = 10000;
+			} else {
+				price = 5000 + random.Next(5000);
+			}
 			return Task.FromResult(new PriceReply {
-				Price = 1000,
+				Price = price,
 				CurrencyCode = "USD"
 			});
 		}
