9f3d9b11ba5ae99108511ff2e2bbd3d47e0d42a1
Dylan Beattie <dylan@dylanbeattie.net>
2021-05-14 12:38:43 +0100

    Added Autobarn.Messages

---
+++ Autobarn/Autobarn.Messages/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Autobarn.Messages {
+	public class NewCarMessage {
+        public string Make { get; set;}
+        public string Model { get;set;}
+        public string Registration { get;set;}
+        public int Year {get;set;}
+        public string Color { get;set;}
+        public DateTimeOffset ListedAt { get;set;}
+	}
+}
