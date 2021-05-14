89690eacd4b4df29cea599c10de8ed94181ad99f
Dylan Beattie <dylan@dylanbeattie.net>
2021-05-14 13:46:03 +0100

    GRPC pricing server and client

---
+++ Autobarn/Autobarn.PricingClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Grpc.Net.Client;
+using Autobarn.PricingServer;
+
+namespace Autobarn.PricingClient {
+	class Program {
+		const string GRPC = "https://workshop.ursatile.com:5003";
+		static void Main(string[] args) {
+			using var channel = GrpcChannel.ForAddress(GRPC);
+			var client = new Pricer.PricerClient(channel);
+			while (true) {
+				var request = new PriceRequest {
+					Color = "blue",
+					Year = 1985,
+					Make = "Ford",
+					Model = "Mustang"
+				};
+				var reply = client.GetPrice(request);
+				Console.WriteLine($"{reply.Price} {reply.CurrencyCode}");
+				Console.ReadKey();
+			}
+		}
+	}
+}
